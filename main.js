1abde7e96e0131a580ee618bfc59c8f3eec920c9
Ismoil98 <ismoil6366>
2024-02-02 15:05:40 +0500

    added

---
+++ main.js
@@ -0,0 +1,148 @@
+// MAP-1 ////////////////////////
+// Map.prototype.deleteMany = function () {
+//   let mp = this;
+//   for (let i = 0; i < arguments.length; i++) {
+//     mp.delete(arguments[i]);
+//   }
+//   return mp;
+// };
+
+// let obj = new Map([
+//   ['a', 1],
+//   ['b', 2],
+//   ['c', 3],
+//   ['d', 4],
+// ]);
+// console.log(obj.deleteMany('a', 'c'));
+
+
+
+// MAP-2 ////////////////////////
+// function mapSum(obj)
+// Map.prototype.deleteMany = function () {
+//     let mp = this;
+//     for (let i = 0; i < arguments.length; i++) {
+//       mp.delete(arguments[i]);
+//     }
+//     return mp;
+// };
+
+
+// MAP-3 ////////////////////////
+// Map.prototype.deleteMany = function () {
+//   let mp = this;
+//   for (let i = 0; i < arguments.length; i++) {
+//     mp.delete(arguments[i]);
+//   }
+//   return mp;
+// };
+
+// let obj = new Map([
+//   ['a', 1],
+//   ['b', 2],
+//   ['c', 3],
+//   ['d', 4],
+// ]);
+// console.log(obj.deleteMany('a', 'c'));
+
+
+
+// MAP-4 ////////////////////////
+// function mapSum(obj)
+// Map.prototype.deleteMany = function () {
+//     let mp = this;
+//     for (let i = 0; i < arguments.length; i++) {
+//       mp.delete(arguments[i]);
+//     }
+//     return mp;
+// };
+
+// MAP-5 ////////////////////////
+// Map.prototype.deleteMany = function () {
+//   let mp = this;
+//   for (let i = 0; i < arguments.length; i++) {
+//     mp.delete(arguments[i]);
+//   }
+//   return mp;
+// };
+
+// let obj = new Map([
+//   ['a', 1],
+//   ['b', 2],
+//   ['c', 3],
+//   ['d', 4],
+// ]);
+// console.log(obj.deleteMany('a', 'c'));
+
+
+
+// MAP-6 ////////////////////////
+// function mapSum(obj)
+// Map.prototype.deleteMany = function () {
+//     let mp = this;
+//     for (let i = 0; i < arguments.length; i++) {
+//       mp.delete(arguments[i]);
+//     }
+//     return mp;
+// };
+
+
+// MAP-7 ////////////////////////
+// Map.prototype.deleteMany = function () {
+//   let mp = this;
+//   for (let i = 0; i < arguments.length; i++) {
+//     mp.delete(arguments[i]);
+//   }
+//   return mp;
+// };
+
+// let obj = new Map([
+//   ['a', 1],
+//   ['b', 2],
+//   ['c', 3],
+//   ['d', 4],
+// ]);
+// console.log(obj.deleteMany('a', 'c'));
+
+
+
+// MAP-8 ////////////////////////
+// function mapSum(obj)
+// Map.prototype.deleteMany = function () {
+//     let mp = this;
+//     for (let i = 0; i < arguments.length; i++) {
+//       mp.delete(arguments[i]);
+//     }
+//     return mp;
+// };
+
+
+
+// MAP-9 ////////////////////////
+// Map.prototype.deleteMany = function () {
+//   let mp = this;
+//   for (let i = 0; i < arguments.length; i++) {
+//     mp.delete(arguments[i]);
+//   }
+//   return mp;
+// };
+
+// let obj = new Map([
+//   ['a', 1],
+//   ['b', 2],
+//   ['c', 3],
+//   ['d', 4],
+// ]);
+// console.log(obj.deleteMany('a', 'c'));
+
+
+
+// MAP-10 ////////////////////////
+// function mapSum(obj)
+// Map.prototype.deleteMany = function () {
+//     let mp = this;
+//     for (let i = 0; i < arguments.length; i++) {
+//       mp.delete(arguments[i]);
+//     }
+//     return mp;
+// };
